447a790788150062453b2564dc819ad2675ee5d2
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2016-09-10 10:49:13 -0400

    creating entity

---
+++ app/src/main/java/com/castrodev/todo/entity/Todo.java
@@ -0,0 +1,52 @@
+package com.castrodev.todo.entity;
+
+import java.util.Date;
+
+/**
+ * Created by rodrigocastro on 10/09/16.
+ */
+
+public class Todo {
+
+    private String name;
+    private Integer status;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Todo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
